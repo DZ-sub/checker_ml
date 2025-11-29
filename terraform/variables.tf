7c2b3ff01e494ffaca442382eb007536d0e59619
Okkyyt <okky.161227@gmail.com>
2025-11-29 23:50:06 +0900

    fix: tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,3 +13,14 @@ provider "aws" {
     access_key = var.aws_access_key
     secret_key = var.aws_secret_key
 }
+
+variable "project_name" {
+    description = "Project name"
+    type        = string
+    default     = "checker-ml-tf"
+}
+
+variable "ecr_image_uri" {
+    description = "ECR Image URI for SageMaker"
+    type        = string
+}
